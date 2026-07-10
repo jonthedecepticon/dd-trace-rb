272c2e1e370557c25032cdc38fc4634f3269da6a
Yury Lebedev <yury.lebedev@datadoghq.com>
2026-07-10 17:40:57 +0200

    Add binding to otel-thread-context

--- ext/libdatadog_api/init.c
+++ ext/libdatadog_api/init.c
@@ -5,6 +5,7 @@
 #include "crashtracker.h"
 #include "feature_flags.h"
 #include "library_config.h"
+#include "otel_thread_ctx.h"
 #include "process_discovery.h"
 
 void ddsketch_init(VALUE core_module);
@@ -23,5 +24,6 @@ void DDTRACE_EXPORT Init_libdatadog_api(void) {
   library_config_init(core_module);
   ddsketch_init(core_module);
   feature_flags_init(core_module);
+  otel_thread_ctx_init(core_module);
   di_init(datadog_module);
 }
